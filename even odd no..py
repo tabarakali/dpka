faa9e1076568c32b34e0f7d13ae0f0f40930e46c
Tabarak ali <tabarakali295@gamil.com>
2019-06-30 12:12:13 +0530

    odd even

---
+++ even odd no..py
@@ -0,0 +1,13 @@
+n = 6
+if n%2==0:
+    print("even=", n)
+else:
+    print("odd=", n)
+i=1
+while i<11:
+     if i % 2 == 0:
+         #print("even=", i)
+         pass
+     else:
+         print("odd=", i)
+     i=i+1
