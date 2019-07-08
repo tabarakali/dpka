b63edeaea5ea5e6fab402e4c88f01e87199fca80
Tabarak ali <tabarakali295@gamil.com>
2019-07-08 13:05:42 +0530

    ifelse

---
+++ ifelseexam..py
@@ -0,0 +1,17 @@
+a=10
+b=0
+if b==0:
+    print("division not poosible")
+else:
+    d= a/b
+    print(d)
+
+
+age = int(input("Enter your age"))
+print(age)
+if age >= 18:
+    print("You are eligible for voter card")
+    print("thank u")
+else:
+    print("abhi nahi banagega")
+
